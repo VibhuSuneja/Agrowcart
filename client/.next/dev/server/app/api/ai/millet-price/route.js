3dd5eb229973c4fa7237b494af73aa1424854216
VibhuSuneja <myrogstrixg17@gmail.com>
2026-01-25 21:50:11 +0530

    Final Polish: ensure consistent project structure

--- client/.next/dev/server/app/api/ai/millet-price/route.js
+++ client/.next/dev/server/app/api/ai/millet-price/route.js
@@ -1,6 +1,9 @@
 var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/ai/millet-price/route.js")
-R.c("server/chunks/node_modules_cebe7f77._.js")
-R.c("server/chunks/[root-of-the-server]__f068acff._.js")
+R.c("server/chunks/node_modules_next_c573a571._.js")
+R.c("server/chunks/node_modules_@auth_core_5e0bf1eb._.js")
+R.c("server/chunks/node_modules_jose_dist_webapi_868b124b._.js")
+R.c("server/chunks/node_modules_d5ffc6cd._.js")
+R.c("server/chunks/[root-of-the-server]__fca1a25d._.js")
 R.c("server/chunks/_next-internal_server_app_api_ai_millet-price_route_actions_621d219d.js")
 R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/ai/millet-price/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
 module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/ai/millet-price/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
